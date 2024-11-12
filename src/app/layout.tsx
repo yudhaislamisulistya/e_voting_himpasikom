73292a4d458f4c2d6746966ae01f66f6be09a33f
waffiq <waffiqmaaroja1998@gmail.com>
2024-11-12 16:26:51 +0700

    update: Migrate to typescript and use app router

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import { ReactNode } from 'react'
+import './globals.css'
+
+const poppins = Poppins({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'Pemilihan Ketua HIMPASIKOM UGM',
+  description:
+    'Program kerja rutin tahunan untuk memilih Ketua HIMPASIKOM UGM ' +
+    'yang baru menggunakan sistem e-voting via website',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  )
+}
